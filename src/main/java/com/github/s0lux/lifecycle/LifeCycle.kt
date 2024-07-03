0c4b895f99184b835a081b9537f9d77385002281
solux <loctranphuoc456@protonmail.com>
2024-07-03 21:14:03 +0700

    Basic DI

--- src/main/java/com/github/s0lux/lifecycle/LifeCycle.kt
+++ src/main/java/com/github/s0lux/lifecycle/LifeCycle.kt
@@ -1,9 +1,20 @@
 package com.github.s0lux.lifecycle
 
 import org.bukkit.plugin.java.JavaPlugin
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import java.util.logging.Logger
 
 class LifeCycle : JavaPlugin() {
     override fun onEnable() {
+        startKoin {
+            modules(
+                module {
+                    single<Logger> { getLogger() }
+                    single<JavaPlugin> { this@LifeCycle }
+            })
+        }
+
         saveDefaultConfig()
     }
 
