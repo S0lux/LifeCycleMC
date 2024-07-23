a05d80f11807f8b01d126037a5e5ece8de79cfba
solux <loctranphuoc456@protonmail.com>
2024-07-23 21:46:38 +0700

    Added comment

--- src/main/java/com/github/s0lux/lifecycle/utils/interfaces/Trait.kt
+++ src/main/java/com/github/s0lux/lifecycle/utils/interfaces/Trait.kt
@@ -7,6 +7,8 @@ import org.bukkit.plugin.java.JavaPlugin
 interface Trait {
     val name: String
     val rarity: Rarity
+
+    // Doesn't do anything yet, maybe in the future it'll do something
     val isHereditary: Boolean
 
     fun apply(player: LifeCyclePlayer, javaPlugin: JavaPlugin)
